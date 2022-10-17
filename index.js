e2985c8de42b0aa058a9efa2af7ea7d10cbaa53b
AmmarEldik <vektorea60@gmail.com>
2022-10-17 23:30:07 +0300

    completed assıgnment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+if (a <= 400){
+  return "This one is on me!";
+}
+else if (a>=400 && a<=2000){
+  return "That will be twenty bucks.";
+}
+ else if (a>2000 && a<2500){
+  return "I will gladly take your thirty bucks.";
+ }
+ else if (a=>2500) {
+  return "No can do."
+ }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city==="NYC"?"Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+   case "generous":
+      return "Thank you so much.";
+      break;
+   case "not as generous":
+        return "Thank you.";
+        break;
+   default:
+          return "Bye."
+  }
 }
